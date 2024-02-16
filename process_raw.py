dc396c00ad8d31d852f823663c74f35058d17027
Mewan Alwis <mewan.alwis@cegeka.com>
2024-02-16 15:51:37 +0100

    read raw file
    code to read raw yellow taxi parquet filr

--- process_raw.py
+++ process_raw.py
@@ -3,3 +3,9 @@ from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
+
+file_path = "/mnt/taxi/yellow.parquet"
+
+df = (spark
+      .read
+      .parquet(file_path))
