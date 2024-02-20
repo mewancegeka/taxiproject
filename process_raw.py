2af468de5ff3d974f66e67c59daf13e659f80586
Mewan Alwis <mewan.alwis@cegeka.com>
2024-02-20 11:20:08 +0100

    write to data lake

--- process_raw.py
+++ process_raw.py
@@ -8,4 +8,14 @@ file_path = "/mnt/taxi/yellow.parquet"
 
 df = (spark
       .read
-      .parquet(file_path))
+      .parquet(file_path)
+      )
+
+df = (df
+      .withColumn("pickup_date",to_date("tpep_pickup_datetime"))
+      .withColumn("travel_time",col("tpep_dropoff_datetime")-col("tpep_pickup_datetime"))
+      .withColumn("taxi_type",lit("yellow"))
+      )
+
+output_directory = "/mnt/silver/taxi"
+df.write.mode("overwrite").partitionBy("pickup_date","taxi_type").parquet(output_directory)
